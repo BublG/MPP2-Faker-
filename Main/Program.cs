4ffa26228fe13f42ec79b5b11697b19cca3cb48c
BublG <vasyapupkin123456123456@mail.ru>
2021-11-02 21:35:27 +0300

    all ready

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,5 +1,8 @@
-﻿using Library.Faker;
+﻿using System;
+using System.Collections.Generic;
+using Library.Faker;
 using Library.Faker.Impl;
+using Main.Classes;
 
 namespace Main
 {
@@ -8,6 +11,18 @@ namespace Main
         public static void Main(string[] args)
         {
             IFaker faker = new Faker();
+            Console.WriteLine(faker.Create<int>());
+            Console.WriteLine(faker.Create<double>());
+            Person p = faker.Create<Person>();
+            Console.WriteLine(p.Age + "  " + p.Name);
+            List<byte> list = faker.Create<List<byte>>();
+            foreach (var b in list)
+            {
+                Console.Write(b + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(faker.Create<DateTime>());
+            //faker.Create<A>();
         }
     }
 }
